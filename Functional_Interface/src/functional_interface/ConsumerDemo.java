00655a1051412f92914d532e411ed5634bcd4453
AjitSurvase <ajitsurvase71@gmail.com>
2026-01-05 16:02:47 +0530

    consumer

---
+++ Functional_Interface/src/functional_interface/ConsumerDemo.java
@@ -0,0 +1,16 @@
+package functional_interface;
+
+import java.util.function.Consumer;
+
+public class ConsumerDemo {
+	
+	public static void main(String[] args) {
+		
+		Consumer<String>c=s->System.out.println(s);
+		
+		c.accept("java 8 comsumer");
+		
+		
+	}
+
+}
